86cfe9b6cef7c1ab2bbf6581e68205df317429e3
DME <pp2913@yahoo.com>
2015-09-21 20:45:32 -0700

    Hi-Low Numbers Game
    Hi-Low Numbers Game.
    Image gallery links to images; Computer finds random number. User gets
    7 tries to guess number.

---
+++ js/gamecode.js
@@ -0,0 +1,19 @@
+
+
+// var tries;
+// for (tries = 1; tries <= 6; tries++) {
+//   alert(index);
+//
+
+//  var for guesses/tries = 0 to start with
+//  you need a var = my guesses
+//
+//  var tries++
+//  if tries === 7 {
+// you loose
+//
+// else {
+//   select another number - alert
+//
+// }
+//  }
